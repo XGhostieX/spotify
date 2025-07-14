5efe73dc5df1f22084008ba64d2dbf7294c0dbc7
XGhostieX <abdallahander85@gmail.com>
2025-07-14 16:39:08 +0300

    project layout

---
+++ lib/core/utils/service_locator.dart
@@ -0,0 +1,14 @@
+import 'package:get_it/get_it.dart';
+
+// import '../../features/home/data/repos/home_repo_impl.dart';
+
+final getIt = GetIt.instance;
+
+void setup() {
+  // getIt.registerSingleton<FirebaseService>(
+  //   FirebaseService(firestore: getIt.get<FirebaseFirestore>()),
+  // );
+  // getIt.registerSingleton<HomeRepoImpl>(
+  //   HomeRepoImpl(getIt.get<FirebaseService>()),
+  // );
+}
---
+++ lib/features/splash/presentation/views_model/splash_cubit/splash_state.dart
@@ -0,0 +1,9 @@
+part of 'splash_cubit.dart';
+
+abstract class SplashState {}
+
+final class SplashDisplay extends SplashState {}
+
+final class Authenticated extends SplashState {}
+
+final class Unauthenticated extends SplashState {}
